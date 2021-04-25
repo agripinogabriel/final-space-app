2a48c5dfbdeb4683fcd274b86dac9b4f844940dd
Agripino Gabriel <agripinogabriel@gmail.com>
2021-04-25 13:08:06 -0300

    feat: Configure AppBar and Background

--- lib/app/pages/episodes/episodes_page.dart
+++ lib/app/pages/episodes/episodes_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../shared/colors.dart';
+
 class EpisodesPage extends StatefulWidget {
   @override
   _EpisodesPageState createState() => _EpisodesPageState();
@@ -9,7 +11,28 @@ class _EpisodesPageState extends State<EpisodesPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      appBar: AppBar(
+        backgroundColor: primaryColor,
+        elevation: 4,
+        title: Hero(
+          tag: "episodes-hero",
+          child:
+              Text("Episodes", style: TextStyle(fontWeight: FontWeight.bold)),
+        ),
+      ),
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: new ExactAssetImage('assets/images/background.jpeg'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
+
 }
