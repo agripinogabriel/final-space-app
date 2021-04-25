ae2904e5fd9a2678b3a24aa7c03cf0761216bc89
Agripino Gabriel <agripinogabriel@gmail.com>
2021-04-25 12:58:30 -0300

    feat: Create route for episodesPage

--- lib/app/route_generator.dart
+++ lib/app/route_generator.dart
@@ -1,26 +1,27 @@
-
 import 'package:flutter/material.dart';
 
 import 'pages/characters/characters_page.dart';
+import 'pages/episodes/episodes_page.dart';
 import 'pages/home/home_page.dart';
 
 class RouteGenerator {
   static const initialRoute = "/";
   static const characters = "/character";
-  static const character = "/character/:id";
+  static const episodes = "/episodes";
 
   static Map<String, Widget Function(BuildContext)> get routes {
     return {
       initialRoute: (context) => HomePage(),
       characters: (context) => CharactersPage(),
-      // character: (context) => CharacterPage(settings.arguments),
+      episodes: (context) => EpisodesPage(),
     };
   }
 
   static Route<dynamic>? onGenerateRoute(RouteSettings settings) {
     final Map<String, WidgetBuilder> _routes = {
       initialRoute: (context) => HomePage(),
-      characters: (context) => CharactersPage()
+      characters: (context) => CharactersPage(),
+      episodes: (context) => EpisodesPage(),
     };
 
     WidgetBuilder? builder = _routes[settings.name];
