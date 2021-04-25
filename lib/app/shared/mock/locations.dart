0bc12a9055d45618ca8376a987fdcd7e6a46e63b
Agripino Gabriel <agripinogabriel@gmail.com>
2021-04-25 16:16:21 -0300

    feat: Add Locations list

---
+++ lib/app/shared/mock/locations.dart
@@ -0,0 +1,75 @@
+const LOCATIONS = [
+  {
+    "id": 1,
+    "name": "Earth",
+    "type": "Planet",
+    "inhabitants": ["Humans", "Animals", "Robots", "Aliens"],
+    "notable_residents": [1, 3],
+    "img_url": "https://finalspaceapi.com/api/location/image/earth.jpg"
+  },
+  {
+    "id": 2,
+    "name": "Final Space",
+    "type": "Dimension",
+    "inhabitants": ["Titans", "Invictus", "Quinn"],
+    "notable_residents": [2, 3, 15, 18],
+    "img_url": "https://finalspaceapi.com/api/location/image/final_space.jpg"
+  },
+  {
+    "id": 3,
+    "name": "Tera Con Prime",
+    "type": "Destroyed planet",
+    "inhabitants": ["Lord Commander's army", "Infinity Guard soldiers"],
+    "notable_residents": [10, 11, 20, 27],
+    "img_url": "https://finalspaceapi.com/api/location/image/tera_con_prime.jpg"
+  },
+  {
+    "id": 4,
+    "name": "Inner Space",
+    "type": "Dimension",
+    "inhabitants": ["Bolo"],
+    "notable_residents": [18],
+    "img_url": "https://finalspaceapi.com/api/location/image/inner_space.jpg"
+  },
+  {
+    "id": 5,
+    "name": "Yarno",
+    "type": "Planet",
+    "inhabitants": [],
+    "notable_residents": [],
+    "img_url": "https://finalspaceapi.com/api/location/image/yarno.jpg"
+  },
+  {
+    "id": 6,
+    "name": "Dark Zone",
+    "type": "Black hole",
+    "inhabitants": [],
+    "notable_residents": [],
+    "img_url": "https://finalspaceapi.com/api/location/image/dark_zone.jpg"
+  },
+  {
+    "id": 7,
+    "name": "Deathcropolis",
+    "type": "Gladiatorial Colosseum",
+    "inhabitants": [],
+    "notable_residents": [],
+    "img_url": "https://finalspaceapi.com/api/location/image/deathcropolis.jpg"
+  },
+  {
+    "id": 8,
+    "name": "Lazarus Trap",
+    "type": "Trap",
+    "inhabitants": [],
+    "notable_residents": [],
+    "img_url": "https://finalspaceapi.com/api/location/image/lazarus_trap.jpg"
+  },
+  {
+    "id": 9,
+    "name": "The Order of the Twelve",
+    "type": "Temple",
+    "inhabitants": [],
+    "notable_residents": [],
+    "img_url":
+        "https://finalspaceapi.com/api/location/image/the_order_of_the_twelve.jpg"
+  }
+];
